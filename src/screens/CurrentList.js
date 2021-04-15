4fc99d9b2ea6ef798958bf7c563ebe77de3238cc
Tantely Andrianarivola <tantely.and@onja.org>
2021-04-15 11:13:52 +0300

    added favorite icons

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,42 +1,21 @@
 import React from 'react';
-
-import {
-  SafeAreaView,
-  Text,
-  ScrollView,
-  TouchableHighlight,
-  StyleSheet,
-} from 'react-native';
-
-import ListItem, {Separator} from './ListItem';
+import {SafeAreaView, ScrollView} from 'react-native';
 import nachos from '../data/nachos';
-
-const styles = StyleSheet.create({
-  touch: {
-    padding: 30,
-  },
-});
-
+import ListItem, {Separator} from './ListItem';
 export default () => {
   return (
     <SafeAreaView>
-      <TouchableHighlight
-        style={styles.touch}
-        activeOpacity={0.6}
-        underlayColor="#DDDDDD"
-        onPress={() => console.log('Hello world')}>
-        <Text> Hello world </Text>
-      </TouchableHighlight>
-
       <ScrollView>
-        {nachos.map(item => {
-          return (
-            <React.Fragment key={item.id}>
-              <ListItem name={item.name} />
-              <Separator />
-            </React.Fragment>
-          );
-        })}
+        {nachos.map((item, index) => (
+          <React.Fragment key={item.id}>
+            <ListItem
+              name={item.name}
+              onFavoritePress={() => alert('todo: handle favorite')}
+              isFavorite={index < 5}
+            />
+            <Separator />
+          </React.Fragment>
+        ))}
       </ScrollView>
     </SafeAreaView>
   );
--- src/screens/ListItem.js
+++ src/screens/ListItem.js
@@ -1,6 +1,66 @@
-import React from 'react';
-import {View, Text, StyleSheet, Image} from 'react-native';
+//import React from 'react';
+//import {
+//  View,
+//  Text,
+//  StyleSheet,
+//  Image,
+//  TouchableOpacity,
+//  onFavoritePress,
+//} from 'react-native';
 
+//const styles = StyleSheet.create({
+//  container: {
+//    padding: 20,
+//    flexDirection: 'row',
+//    backgroundColor: '#fff',
+//    justifyContent: 'space-between',
+//    alignItems: 'center',
+//  },
+
+//  text: {
+//    fontSize: 18,
+//    color: '#69696969',
+//  },
+
+//  icon: {
+//    height: 30,
+//    tintColor: '#69696969',
+//  },
+//  separator: {
+//    flex: 1,
+//    height: 1,
+//    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+//  },
+//});
+
+//export const Separator = () => <View style={styles.separator} />;
+
+//const ListItem = ({name}) => {
+//  return (
+//    <View style={styles.container}>
+//      <Text style={styles.text}>{name} </Text>
+//      {onFavoritePress && (
+//        <TouchableOpacity onPress={onFavoritePress}>
+//          <Image
+//            source={require('../assets/icons/ios-star-outline.png')}
+//            style={styles.icon}
+//            resizeMode="contain"
+//          />
+//        </TouchableOpacity>
+//      )}
+//    </View>
+//  );
+//};
+
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     padding: 20,
@@ -9,15 +69,17 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
   },
-
   text: {
     fontSize: 18,
     color: '#69696969',
   },
-
   icon: {
     height: 30,
     tintColor: '#69696969',
+    ...Platform.select({
+      ios: {tintColor: 'blue'},
+      android: {tintColor: 'red'},
+    }),
   },
   separator: {
     flex: 1,
@@ -25,20 +87,30 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0, 0, 0, 0.2)',
   },
 });
-
 export const Separator = () => <View style={styles.separator} />;
+const ListItem = ({name, onFavoritePress, isFavorite}) => {
+  let starIcon;
+  if (isFavorite) {
+    starIcon = Platform.select({
+      ios: require('../assets/icons/ios-star.png'),
+      android: require('../assets/icons/md-star.png'),
+    });
+  } else {
+    starIcon = Platform.select({
+      ios: require('../assets/icons/ios-star-outline.png'),
+      android: require('../assets/icons/md-star-outline.png'),
+    });
+  }
 
-const ListItem = ({name}) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{name} </Text>
-      <Image
-        source={require('../assets/icons/ios-star-outline.png')}
-        style={styles.icon}
-        resizeMode="contain"
-      />
+      <Text style={styles.text}>{name}</Text>
+      {onFavoritePress && (
+        <TouchableOpacity onPress={onFavoritePress}>
+          <Image source={starIcon} style={styles.icon} resizeMode="contain" />
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
-
 export default ListItem;
