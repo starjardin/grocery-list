7c725e4794557c8a378e56d7ebb8436824e3fd32
Tantely Andrianarivola <tantely.and@onja.org>
2021-04-15 11:30:03 +0300

    modified scrolling to faltlist

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
+import {SafeAreaView, ScrollView, FlatList} from 'react-native';
 import nachos from '../data/nachos';
 import ListItem, {Separator} from './ListItem';
 export default () => {
   return (
     <SafeAreaView>
-      <ScrollView>
-        {nachos.map((item, index) => (
-          <React.Fragment key={item.id}>
-            <ListItem
-              name={item.name}
-              onFavoritePress={() => alert('todo: handle favorite')}
-              isFavorite={index < 5}
-            />
-            <Separator />
-          </React.Fragment>
-        ))}
-      </ScrollView>
+      <FlatList
+        data={nachos}
+        renderItem={({item, index}) => (
+          <ListItem
+            name={item.name}
+            onFavoritePress={() => alert('todo: handle favorite!')}
+            isFavorite={index < 5}
+          />
+        )}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={() => <Separator />}
+      />
     </SafeAreaView>
   );
+
+  //return (
+  //  <SafeAreaView>
+  //    <ScrollView>
+  //      {nachos.map((item, index) => (
+  //        <React.Fragment key={item.id}>
+  //          <ListItem
+  //            name={item.name}
+  //            onFavoritePress={() => alert('todo: handle favorite')}
+  //            isFavorite={index < 5}
+  //          />
+  //          <Separator />
+  //        </React.Fragment>
+  //      ))}
+  //    </ScrollView>
+  //  </SafeAreaView>
+  //);
 };
