de3e974e59f2ab89f5dbc68c2a9c677742801a65
Tantely Andrianarivola <tantely.and@onja.org>
2021-04-16 11:26:00 +0300

    added item to localstorage

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,21 +1,54 @@
-import React, {useState} from 'react';
-import {SafeAreaView, FlatList, KeyboardAvoidingView} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  KeyboardAvoidingView,
+  ActivityIndicator,
+} from 'react-native';
 import {v4 as uuid} from 'uuid';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import nachos from '../data/nachos';
+
+const updateStorageCurrentList = list => {
+  AsyncStorage.setItem('@@GreceryList/CurrentList', JSON.stringify(list));
+};
+
 import ListItem, {Separator} from './ListItem';
 import AddItem from './AddItem';
 export default () => {
-  const [list, setList] = useState(nachos);
+  const [list, setList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const addItem = text => {
-    setList([{id: uuid(), name: text}, ...list]);
+    const newList = [{id: uuid(), name: text}, ...list];
+    setList(newList);
+    updateStorageCurrentList(newList);
+    setList();
   };
 
   const removeItem = id => {
     const newList = list.filter(item => item.id !== id);
     setList(newList);
+    updateStorageCurrentList(newList);
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      AsyncStorage.getItem('@@GreceryList / CurrentList')
+        .then(data => JSON.parse(data))
+        .then(data => {
+          if (data) {
+            setList(data);
+          }
+          setIsLoading(false);
+        });
+    }, 2000);
+  });
+
+  //if (isLoading) {
+  //  return <SafeAreaView>Loading....</SafeAreaView>;
+  //}
 
   return (
     <SafeAreaView style={{flex: 1}}>
